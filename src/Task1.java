ddac39c7b39ed7e7b5f6f1cc5b1dbd64cc25992f
Elena <aleklenoch@gmail.com>
2023-11-27 11:29:23 +0200

    Task1

---
+++ src/Task1.java
@@ -0,0 +1,5 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("\"Your time is limited,\n\tso don’t waste it\n\t\tliving someone else’s life\"\n\t\t\tSteve Jobs");
+    }
+}
