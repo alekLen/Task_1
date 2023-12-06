b5a7d31866d379979108290dbecb5ddd379585cd
Elena <aleklenoch@gmail.com>
2023-12-06 11:40:37 +0200

    Class Product

---
+++ src/ExtendsClassesPackage/Money.java
@@ -0,0 +1,45 @@
+package ExtendsClassesPackage;
+
+public  class Money {
+    private int dollars=0;
+    private int cents=0;
+
+    public Money(int dollars, int cents) {
+        setDollars(dollars);
+        setCents(cents);
+    }
+
+    public  void display() {
+        System.out.printf("%d.%02d%n", dollars, cents);
+    }
+
+    public void setAmount(int dollars, int cents) {
+        setDollars(dollars);
+        setCents(cents);
+    }
+
+    public int getDollars() {
+        return dollars;
+    }
+
+    public int getCents() {
+        return cents;
+    }
+
+    public void setDollars(int dollars) {
+        this.dollars = Math.max(dollars, 0);
+    }
+
+    public void setCents(int cents) {
+        if(cents > 99) {
+            int n1 = cents % 100;
+            this.cents = n1;
+            n1 = cents / 100;
+            this.dollars += n1;
+        } else if(cents<0){
+            this.cents =0;
+        } else{
+            this.cents = cents;
+        }
+    }
+}
---
+++ src/ExtendsClassesPackage/Product.java
@@ -0,0 +1,65 @@
+package ExtendsClassesPackage;
+
+public class Product extends Money{
+     private String name = "";
+    public Product(String name,int dollars, int cents) {
+        super(dollars, cents);
+        this.name=name;
+    }
+
+    public Product() {
+        super(0, 0);
+    }
+
+    public void upPrice(double price){
+        int n1= (int) price;
+        super.setDollars(super.getDollars() + n1);
+       double n2= price-n1;
+       n2=(Math.round(n2 * 100) );
+        n1= (int) n2;
+        super.setCents(super.getCents() + n1);
+    }
+    public void downPrice(double price){
+        int n1= (int) price;
+        super.setDollars(super.getDollars() - n1);
+        double n2= price-n1;
+        n2=(Math.round(n2 * 100) );
+        n1= (int) n2;
+        if(super.getCents() >= n1) {
+            super.setCents(super.getCents() - n1);
+        } else{
+            if(super.getDollars()==0){
+                super.setCents(0);
+            } else {
+                super.setDollars(super.getDollars() - 1);
+                super.setCents(100 + super.getCents() - n1);
+            }
+        }
+    }
+    public void print(){
+        System.out.print(name+" цена: ");
+        super.display();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getDollars() {
+        return super.getDollars();
+    }
+
+    public void setDollars(int dollars) {
+        super.setDollars(dollars);
+    }
+    public void setCents(int cents) {
+        super.setCents(cents);
+    }
+    public int getCents() {
+        return super.getCents();
+    }
+
+}
