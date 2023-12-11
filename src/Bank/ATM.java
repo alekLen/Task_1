b12f5e0a8bdbee49827ee8ce02c470fe8efff861
Elena <aleklenoch@gmail.com>
2023-12-11 18:46:13 +0200

    giveOutMoney

--- src/Bank/ATM.java
+++ src/Bank/ATM.java
@@ -1,11 +1,14 @@
 package Bank;
 
 import Bank.MyExeptions.MinSummToGiveExeption;
+import Bank.MyExeptions.NotInafMoneyInBox;
 import Bank.MyExeptions.OutOfMaxSummOfMoneyBoxExeption;
 import Bank.MyExeptions.OutOfMinSummOfMoneyBoxExeption;
+import Bank.MyExeptions.OnlyHaveSummaInBox;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ATM {
 public List<MonyeBox> moneyBoxes = new ArrayList<>();
@@ -31,10 +34,10 @@ public List<MonyeBox> moneyBoxes = new ArrayList<>();
       moneyBoxes.add(m);
    }
 
-   public void minusCount(int n1, MonyeBox mb) {
-      int newCount = mb.getCount()-n1;
+   public void minusCount( MonyeBox mb, int number) {
+      int newCount = mb.getCount()-1;
       if(newCount < 0 ){
-         throw new OutOfMinSummOfMoneyBoxExeption();
+         throw new NotInafMoneyInBox(number);
       }
       else{
          mb.setCount(newCount);
@@ -50,10 +53,70 @@ public List<MonyeBox> moneyBoxes = new ArrayList<>();
       }
    }
    public void giveOutMoney(int n){
-      int count=0;
-      if( n < minSummOut){
-         throw new MinSummToGiveExeption(minSummOut);
-      }
-
+       try {
+          if( n < minSummOut){
+            throw new MinSummToGiveExeption(minSummOut);
+           }
+          else{
+              int giveOutSumma = takeMoneyFromBox(n, 500);
+               giveOutSumma = takeMoneyFromBox(n - giveOutSumma, 200);
+               giveOutSumma = takeMoneyFromBox(n - giveOutSumma, 100);
+               giveOutSumma = takeMoneyFromBox(n - giveOutSumma, 50);
+               giveOutSumma = takeMoneyFromBox(n - giveOutSumma, 20);
+               giveOutSumma = takeMoneyFromBox(n - giveOutSumma, 10);
+               giveOutSumma = takeMoneyFromBox(n - giveOutSumma, 5);
+               if(giveOutSumma!=n){
+                   System.out.println("Возможно выдать только сумму:  " + giveOutSumma);
+                   System.out.println("Подтвердите выдачу этой суммы: нажмите 1");
+                   try{
+                       Scanner scanner = new Scanner(System.in);
+                      int choise= scanner.nextInt();
+                      if(choise==1){
+                          System.out.println("Заберите деньги в сумме: " + giveOutSumma);
+                      }
+                   }
+                   catch(Exception e) {
+                       System.out.println("Отмена операции!");
+                   }
+               }
+               else{
+                   System.out.println("Возьмите свои деньги в сумме:  " + giveOutSumma);
+               }
+          }
+       }
+       catch (MinSummToGiveExeption e){
+           System.out.println(e.getMessage());
+       }
    }
+   public int takeMoneyFromBox(int summa, int nominal){
+       int giveOutSumma = 0;
+       int number1 = summa/nominal;
+       if(number1 > 0){
+           MonyeBox monyebox = findMoneyBox(nominal);
+           if (monyebox != null){
+               boolean have = true;
+               int numberCoupure = 0;
+               while(number1 > 0 && have) {
+                   try {
+                       minusCount(monyebox,numberCoupure);
+                       numberCoupure++;
+                       number1--;
+                   } catch (NotInafMoneyInBox e) {
+                       numberCoupure=e.getCount();
+                       have=false;
+                   }
+               }
+               giveOutSumma += numberCoupure * nominal;
+           }
+       }
+       return giveOutSumma;
+   }
+    public MonyeBox findMoneyBox(int nominal){
+        for (var m :moneyBoxes ) {
+            if(m.getNominal()==nominal){
+                return m;
+            }
+        }
+        return null;
+    }
 }
---
+++ src/Bank/MyExeptions/NotInafMoneyInBox.java
@@ -0,0 +1,18 @@
+package Bank.MyExeptions;
+
+public class NotInafMoneyInBox  extends RuntimeException{
+    private int count;
+    public NotInafMoneyInBox(int number) {
+        count=number;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Нет денег в контейнере";
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
