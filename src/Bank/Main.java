a51de532e620cd347f3c98acd18476d8c83353d7
Elena <aleklenoch@gmail.com>
2023-12-12 16:04:43 +0200

    fix

--- src/Bank/Main.java
+++ src/Bank/Main.java
@@ -5,8 +5,8 @@ import Bank.MyExeptions.ExceptionOfCreatingBank;
 public class Main {
     public static void main(String[] args) {
 try {
-   Bank bank = new Bank(3, 2300325, 1000, 20000);
-  //  Bank bank = new Bank(2, 800000, 1000, 20000);
+    Bank bank = new Bank(3, 2300325, 1000, 20000);
+   // Bank bank = new Bank(2, 800000, 1000, 20000);
     int c=bank.getCountAtm();
     System.out.println("Создан банк. Создано  "+ c +" банкомата");
     int summa = bank.CurentSummInAllAtms();
@@ -14,22 +14,32 @@ try {
     bank.list.get(1).giveOutMoney(10200);
      summa = bank.CurentSummInAllAtms();
     System.out.println("Общая сумма в банке:  " + summa);
+    System.out.println("     *******");
 
     bank.list.get(1).giveOutMoney(23220);
+    summa = bank.CurentSummInAllAtms();
+    System.out.println("Общая сумма в банке:  " + summa);
+    System.out.println("     *******");
+
     bank.list.get(1).giveOutMoney(13270);
         summa = bank.CurentSummInAllAtms();
         System.out.println("Общая сумма в банке:  " + summa);
+    System.out.println("     *******");
 
     bank.list.get(1).putInMoneyBox(20,500);
         summa = bank.CurentSummInAllAtms();
         System.out.println("Общая сумма в банке:  " + summa);
+    System.out.println("     *******");
 
     bank.list.get(1).putInMoneyBox(320,500);
         summa = bank.CurentSummInAllAtms();
         System.out.println("Общая сумма в банке:  " + summa);
+    System.out.println("     *******");
+
     bank.list.get(1).putInMoneyBox(20,50);
     summa = bank.CurentSummInAllAtms();
     System.out.println("Общая сумма в банке:  " + summa);
+    System.out.println("     *******");
 
 }
 catch(ExceptionOfCreatingBank e) {
